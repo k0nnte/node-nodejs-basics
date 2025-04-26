5816b6b169eda9257c402d9b3d0b0041fd0689a6
k0nnte <bystrov.igor25@gmail.com>
2025-04-26 05:47:36 +0300

    feat: complite create copy and rename js "

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,31 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const rename = async () => {
-    // Write your code here 
+    const oldFile= path.resolve(__dirname, 'files', 'wrongFilename.txt');
+    const newFile = path.resolve(__dirname, 'files' ,'properFilename.md');
+
+    try{
+        await fs.access(oldFile);
+    }catch(err){
+        throw new Error('FS operation failed');
+    }
+
+    try{
+        await fs.access(newFile)
+        throw new Error('FS operation failed');
+    }catch(err){
+        if(err.code !== 'ENOENT'){
+            throw err;
+        }
+    }
+
+    await fs.rename(oldFile, newFile);
 };
 
 await rename();
