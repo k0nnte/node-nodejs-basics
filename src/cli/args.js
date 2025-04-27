9470c916220e6969ee7a312830a1bbbac723994e
k0nnte <bystrov.igor25@gmail.com>
2025-04-27 04:46:43 +0300

    feat: add cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-    // Write your code here 
+   const args = process.argv.slice(2); 
+   
+    const rez = [];
+    for (let i = 0; i < args.length; i += 2) {
+        const key = args[i].replace(/^--/, '');
+        const value = args[i + 1];
+        rez.push(`${key} is ${value}`);
+    }
+    console.log(rez);
+   
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const env = Object.entries(process.env).filter(([k])=> k.startsWith('RSS_')).map(([k,v])=> `${k}=${v}`).join('; ');
+    console.log(env);
+    
+    
+    
 };
 
 parseEnv();
